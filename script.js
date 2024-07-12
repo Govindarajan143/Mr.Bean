419d43c9a17de18330d6fd5666e034b562f8766e
Govindarajan143 <rajg84682@gmail.com>
2024-07-12 05:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const balls = document.getElementsByClassName('ball');
-document.onmousemove = (a) =>{
-    const X = (a.clientX * 100) / window.innerWidth + '%';
-    const Y = (a.clientY * 100) / window.innerHeight + '%';
+
+document.onmousemove=(a)=>{
+    const x=(a.clientX * 100)/window.innerWidth+'%';
+    const y=(a.clientY * 100)/window.innerHeight+'%';
+    
 for(i=0;i<100;i++){
-    balls[i].style.left = X; 
-    balls[i].style.top = Y; 
-    balls[i].transform = 'translate(-'+ X +',-'+ Y +')';
+    balls[i].style.left=x; 
+    balls[i].style.top=y; 
+    balls[i].transform='translate(-'+x+',-'+y+')';
 }
 };
