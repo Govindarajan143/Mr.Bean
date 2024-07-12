b9fffb57adad64b5ff3cb0c924d552819b5aa176
Govindarajan143 <rajg84682@gmail.com>
2024-07-12 05:51:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const balls = document.getElementsByClassName('ball');
+document.onmousemove = (a) =>{
+    const X = (a.clientX * 100) / window.innerWidth + '%';
+    const Y = (a.clientY * 100) / window.innerHeight + '%';
+for(i=0;i<100;i++){
+    balls[i].style.left = X; 
+    balls[i].style.top = Y; 
+    balls[i].transform = 'translate(-'+ X +',-'+ Y +')';
+}
+};
